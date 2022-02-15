ded68a3d8d7f0594d47438ed0befc26b7b2fcdab
Bit7eg <stonysyrop@gmail.com>
2022-02-15 20:37:23 +0700

    Task 3 done. Task 2 modified.

--- Task2/Task2.c
+++ Task2/Task2.c
@@ -3,8 +3,8 @@
 
 int main()
 {
-    const double step = 3.0 / 20;
-    double sum = 0;
+	const int accuracy = 20;
+	double sum = 0, step = 3.0 / accuracy;
 	for (double i = -1 + step / 2; i < 2; i += step)
 	{
 		sum += (i / ((i * i + 1) + (i * i + 1))) * step;
---
+++ Task3/Task3.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int minute, hour, passed_minutes = 0;
+    scanf("%i%i", &hour, &minute);
+    hour = (hour * 5 + minute / 12) % 60;
+    while (hour != minute)
+    {
+        minute++;
+        passed_minutes++;
+        if (minute==60)
+        {
+            minute = 0;
+        }
+        if (minute % 12 == 0)
+        {
+            hour++;
+        }
+        if (hour == 60)
+        {
+            hour = 0;
+        }
+    }
+    hour = (hour - minute / 12) / 5;
+    printf("\n%ih %im\n", hour, minute);
+    printf("%i minutes passed\n", passed_minutes);
+}
