70d30bda216fa142e475affdafe5044199c46824
Bit7eg <stonysyrop@gmail.com>
2022-02-15 20:50:57 +0700

    Corrected mistake

--- Task2/Task2.c
+++ Task2/Task2.c
@@ -7,7 +7,7 @@ int main()
 	double sum = 0, step = 3.0 / accuracy;
 	for (double i = -1 + step / 2; i < 2; i += step)
 	{
-		sum += (i / ((i * i + 1) + (i * i + 1))) * step;
+		sum += (i / ((i * i + 1) * (i * i + 1))) * step;
 	}
 	printf("%f", sum);
 }
