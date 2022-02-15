b36096e263b079e3a43d61799b16ecead6b5b23a
Bit7eg <stonysyrop@gmail.com>
2022-02-15 19:34:43 +0700

    Task2 done

---
+++ Task2/Task2.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    const double step = 3.0 / 20;
+    double sum = 0;
+	for (double i = -1 + step / 2; i < 2; i += step)
+	{
+		sum += (i / ((i * i + 1) + (i * i + 1))) * step;
+	}
+	printf("%f", sum);
+}
