1ec335c9b7875deaa29005fd47799d594ad6bb58
i.stepanko <i.stepanko@WS4214-03>
2022-02-16 12:25:27 +0700

    Created ThemeGraph project

---
+++ ThemeGraph/GraphLib.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include "GraphLib.h"
+
+GRAPH initEmptyGraph(int nodeCount) {
+	GRAPH graph;
+	graph = calloc(nodeCount, sizeof(short int*));
+	for (size_t i = 0; i < nodeCount; i++)
+	{
+		graph[i] = calloc(nodeCount, sizeof(short int));
+		for (size_t j = 0; j < nodeCount; j++)
+		{
+			graph[i][j] = 0;
+		}
+	}
+	return graph;
+}
+
+int getGraphLength(GRAPH graph) {
+	int sqrLength;
+	sqrLength = sizeof(graph[0]) / sizeof(short int);
+	return sqrt(sqrLength);
+}
+
+void killGraph(GRAPH graph) {
+	for (size_t i = 0; i < getGraphLength(graph); i++)
+	{
+		free(graph[i]);
+	}
+	free(graph);
+}
+
+GRAPH addNodes(GRAPH graph, int nodeCount) {
+	int resLength, graphLength;
+	graphLength = getGraphLength(graph);
+	resLength = graphLength + nodeCount;
+	if (resLength < graphLength) {
+		resLength = INT_MAX;
+	}
+	graph = realloc(graph, resLength * sizeof(short int*));
+	for (size_t i = 0; i < graphLength; i++)
+	{
+		graph[i] = realloc(graph[i], resLength * sizeof(short int));
+		for (size_t j = graphLength; j < resLength; j++)
+		{
+			graph[i][j] = 0;
+		}
+	}
+	for (size_t i = graphLength; i < resLength; i++)
+	{
+		graph[i] = realloc(graph[i], resLength * sizeof(short int));
+		for (size_t j = 0; j < resLength; j++)
+		{
+			graph[i][j] = 0;
+		}
+	}
+	return graph;
+}
+
+GRAPH removeNodes(GRAPH graph, int nodeCount) {
+	int resLength;
+	resLength = getGraphLength(graph) - nodeCount;
+	GRAPH newGraph;
+	newGraph = NULL;
+	if (resLength > 0) {
+		newGraph = calloc(resLength, sizeof(short int*));
+		for (size_t i = 0; i < resLength; i++)
+		{
+			newGraph[i] = calloc(resLength, sizeof(short int));
+			for (size_t j = 0; j < resLength; j++)
+			{
+				newGraph[i][j] = graph[i][j];
+			}
+		}
+	}
+	killGraph(graph);
+	return newGraph;
+}
+
+void printGraph(GRAPH graph) {
+	int size = getGraphLength(graph);
+	printf("    ");
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("%3i ", i);
+	}
+	printf("\n");
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("%3i ", i);
+		for (size_t j = 0; j < size; j++)
+		{
+			printf("%3hi ", graph[i][j]);
+		}
+		printf("\n");
+	}
+}
---
+++ ThemeGraph/ThemeGraph.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include "GraphLib.h"
+
+void pow3noCycle3(GRAPH graph, size_t size)
+{
+    if (size > 3)
+    {
+        graph[size - 1][size - 2] = 1;
+        graph[size - 1][size - 1] = 1;
+        graph[size - 2][size - 1] = 1;
+        graph[size - 2][size - 2] = 1;
+        pow3noCycle3(graph, size - 2);
+    }
+    else if (size == 3) {
+
+    }
+    else if (size == 2)
+    {
+        graph[0][1] = 1;
+        graph[0][0] = 1;
+        graph[1][0] = 1;
+        graph[1][1] = 1;
+    }
+    else
+    {
+        graph = NULL;
+    }
+}
+
+int main()
+{
+    int nodeCount = 1;
+    scanf("%i", &nodeCount);
+    GRAPH graph = initEmptyGraph(nodeCount);
+    pow3noCycle3(graph, nodeCount);
+    printGraph(graph);
+}
