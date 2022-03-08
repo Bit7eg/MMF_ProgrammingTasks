7d519e59de737b40a3f2d38569a58062c7629d98
Bit7eg <stonysyrop@gmail.com>
2022-03-08 18:04:19 +0700

    Task7 done

---
+++ Task7/Task7.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include "GraphLib.h"
+
+int main()
+{
+    int nodeCount;
+    scanf("%i", &nodeCount);
+    GRAPH graph = initEmptyGraph(nodeCount);
+    int fromVertex = 0, toVertex = 0, dist = 0;
+    scanf("%i%i%i", &fromVertex, &toVertex, &dist);
+    while ((fromVertex > 0) && (toVertex > 0) && (dist > 0))
+    {
+        graph[(--fromVertex)][(--toVertex)] = dist;
+        scanf("%i%i%i", &fromVertex, &toVertex, &dist);
+    }
+    GRAPH distMatrix = getDistanceMatrix(graph);
+    printf("\n");
+    printGraph(distMatrix);
+    killGraph(distMatrix);
+    killGraph(graph);
+}
