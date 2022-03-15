700b0ed31f244f5acb3160ecf58058ef8dfe30cb
Bit7eg <stonysyrop@gmail.com>
2022-03-15 22:42:57 +0700

    Improved some functions. Added initBinGraph(), getVariableSet(), logicalFragmentResult() and scanBinGraph() functions. BinGraph lib depend of ValueSet lib

--- AdditionalFiles/BinGraphLib.c
+++ AdditionalFiles/BinGraphLib.c
@@ -1,39 +1,198 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "BinGraphLib.h"
+#include "ValueSetLib.h"
 
-int *a[2];
-
-BIN_GRAPH initEmptyBinGraph(int nodeCount)
-{
-
+BIN_GRAPH initBinGraph() {
+	BIN_GRAPH graph = malloc(sizeof(node));
+	graph->mark = '\0';
+	graph->minuseNode = NULL;
+	graph->plusNode = NULL;
+	return graph;
 }
 
-BIN_GRAPH initRandomBinGraph(int nodeCount)
-{
-
+int getNodeCount(BIN_GRAPH graph) {
+	unsigned int length = 0;
+	while ((graph[length].mark != '\0') ||
+		(graph[length].minuseNode != NULL) ||
+		(graph[length].plusNode != NULL))
+	{
+		length++;
+	}
+	return length;
 }
 
-int getBinGraphLength(BIN_GRAPH graph)
-{
-
+void killBinGraph(BIN_GRAPH graph) {
+	free(graph);
 }
 
-void killBinGraph(BIN_GRAPH graph)
-{
-
+int isLeaf(node vertex) {
+	if ((vertex.mark >= '0') && (vertex.mark <= '9') && (vertex.minuseNode == NULL) && (vertex.plusNode == NULL))
+	{
+		return 1;
+	}
+	else if ((vertex.mark >= 'A') && (vertex.mark <= 'Z') && (vertex.minuseNode != NULL) && (vertex.plusNode != NULL))
+	{
+		return 0;
+	}
+	else
+	{
+		return -1;
+	}
 }
 
-BIN_GRAPH addNodesBin(BIN_GRAPH graph, int nodeCount)
-{
-
+int isLogicalFragment(BIN_GRAPH graph) {
+	int size = getNodeCount(graph);
+	for (size_t i = 0; i < size; i++)
+	{
+		if (isLeaf(graph[i]) == -1)
+		{
+			return 0;
+		}
+	}
+	return 1;
 }
 
-BIN_GRAPH removeNodesBin(BIN_GRAPH graph, int nodeCount)
-{
-
+node* getNodePointer(BIN_GRAPH graph, char mark) {
+	int size = getNodeCount(graph);
+	for (size_t i = 0; i < size; i++)
+	{
+		if (graph[i].mark==mark)
+		{
+			return graph + i;
+		}
+	}
+	return NULL;
 }
 
-void printBinGraph(BIN_GRAPH graph)
-{
+BIN_GRAPH addNodesBin(BIN_GRAPH graph, char mark) {
+	int size = getNodeCount(graph) + 1;
+	graph = realloc(graph, (size+1) * sizeof(node));
+	graph[size - 1].mark = mark;
+	graph[size - 1].minuseNode = NULL;
+	graph[size - 1].plusNode = NULL;
+	graph[size].mark = '\0';
+	graph[size].minuseNode = NULL;
+	graph[size].plusNode = NULL;
+	return graph;
+}
 
+VALUE_SET getVariableSet(BIN_GRAPH graph) {
+	VALUE_SET vars = initValueSet();
+	varValue var;
+	int size = getNodeCount(graph);
+	var.value = 0;
+	for (size_t i = 0; i < size; i++)
+	{
+		if ((graph[i].mark >= 'A') && (graph[i].mark <= 'Z'))
+		{
+			var.name = graph[i].mark;
+			vars = addVar(vars, var);
+		}
+	}
+	return vars;
+}
+
+char logicalFragmentResult(BIN_GRAPH graph, VALUE_SET vars) {
+	int graph_size = getNodeCount(graph), set_size = getVarCount(vars);
+	varValue *varPointer, variable;
+	node* vertex;
+	if (!isLogicalFragment(graph))
+	{
+		return '\0';
+	}
+	VALUE_SET passedVertex = initValueSet();
+	variable.value = 0;
+	for (size_t i = 0; i < graph_size; i++)
+	{
+		if (!isLeaf(graph[i]))
+		{
+			varPointer = getVarPointerByName(vars, graph[i].mark);
+			if (varPointer == NULL)
+			{
+				killValueSet(passedVertex);
+				return '\0';
+			}
+			if ((varPointer->value != 1) && (varPointer->value != 0))
+			{
+				killValueSet(passedVertex);
+				return '\0';
+			}
+			variable.name = graph[i].mark;
+			passedVertex = addVar(passedVertex, variable);
+		}
+	}
+	vertex = graph;
+	varPointer = getVarPointerByName(passedVertex, vertex->mark);
+	while (!isLeaf(*vertex))
+	{
+		if (varPointer->value)
+		{
+			killValueSet(passedVertex);
+			return ('9' + 1);
+		}
+		varPointer->value = 1;
+		varPointer = getVarPointerByName(vars, vertex->mark);
+		if (varPointer->value)
+		{
+			vertex = vertex->plusNode;
+		}
+		else
+		{
+			vertex = vertex->minuseNode;
+		}
+		varPointer = getVarPointerByName(passedVertex, vertex->mark);
+	}
+	killValueSet(passedVertex);
+	return vertex->mark;
+}
+
+void scanBinGraph(BIN_GRAPH graph) {
+	char mark, plus, minuse;
+	node* vmark, * vplus, * vminuse;
+	scanf("%c%c%c", &mark, &plus, &minuse);
+	while ((mark >= 'A') && (mark <= 'Z'))
+	{
+		vmark = getNodePointer(graph, mark);
+		if (vmark == NULL)
+		{
+			graph = addNodesBin(graph, mark);
+			vmark = getNodePointer(graph, mark);
+		}
+		vplus = getNodePointer(graph, plus);
+		if (vplus == NULL)
+		{
+			graph = addNodesBin(graph, plus);
+			vplus = getNodePointer(graph, plus);
+		}
+		vminuse = getNodePointer(graph, minuse);
+		if (vminuse == NULL)
+		{
+			graph = addNodesBin(graph, minuse);
+			vminuse = getNodePointer(graph, minuse);
+		}
+		vmark->minuseNode = vminuse;
+		vmark->plusNode = vplus;
+		scanf("%c%c%c", &mark, &plus, &minuse);
+	}
+}
+
+void printBinGraph(BIN_GRAPH graph) {
+	int size = getNodeCount(graph);
+	printf("      ");
+	for (size_t i = 0; i < size; i++)
+	{
+		printf(" %c", graph[i].mark);
+	}
+	printf("\nplus  ");
+	for (size_t i = 0; i < size; i++)
+	{
+		printf(" %c", graph[i].plusNode->mark);
+	}
+	printf("\nminuse");
+	for (size_t i = 0; i < size; i++)
+	{
+		printf(" %c", graph[i].minuseNode->mark);
+	}
+	printf("\n");
 }
--- AdditionalFiles/BinGraphLib.h
+++ AdditionalFiles/BinGraphLib.h
@@ -2,15 +2,25 @@
 #ifndef BINGRAPHLIB_H
 #define BINGRAPHLIB_H
 
+#include "ValueSetLib.h"
+
 typedef struct node {
-	int plusNode = -1;
-	int minuseNode = -1;
+	char mark;
+	struct node* plusNode;
+	struct node* minuseNode;
 } node;
 typedef node* BIN_GRAPH;
 
-int getAlphabetLength(BIN_GRAPH);
+BIN_GRAPH initBinGraph();
+int getNodeCount(BIN_GRAPH);
 void killBinGraph(BIN_GRAPH);
-BIN_GRAPH addNodesBin(int, int, int);
+int isLeaf(node);
+int isLogicalFragment(BIN_GRAPH);
+node* getNodePointer(BIN_GRAPH, char);
+BIN_GRAPH addNodesBin(BIN_GRAPH, char);
+VALUE_SET getVariableSet(BIN_GRAPH);
+char logicalFragmentResult(BIN_GRAPH, VALUE_SET);
+void scanBinGraph(BIN_GRAPH);
 void printBinGraph(BIN_GRAPH);
 
 #include "BinGraphLib.c"
