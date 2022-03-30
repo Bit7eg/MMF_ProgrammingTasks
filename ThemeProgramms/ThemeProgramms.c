e84326447038272bfc78aa824011f1c4e14e5a07
i.stepanko <i.stepanko@WS4214-03>
2022-03-30 12:19:52 +0700

    Begin making tests

--- ThemeProgramms/ThemeProgramms.c
+++ ThemeProgramms/ThemeProgramms.c
@@ -2,15 +2,9 @@
 #include <string.h>
 #include "BinGraphLib.h"
 
-int main()
-{
-    char* fileName;
-    printf("First fragment file name: ");
-    scanf("%s", &fileName);
-    BIN_GRAPH fragment1 = fscanBinGraph(fileName);
-    printf("Second fragment file name: ");
-    scanf("%s", &fileName);
-    BIN_GRAPH fragment2 = fscanBinGraph(fileName);
+char* executeProgram(const char* fileName1, const char* fileName2) {
+    BIN_GRAPH fragment1 = fscanBinGraph(fileName1);
+    BIN_GRAPH fragment2 = fscanBinGraph(fileName2);
     VALUE_SET vars = getVariableSet(fragment1), varsBuff = getVariableSet(fragment2);
     if (getVarCount(vars) >= getVarCount(varsBuff))
     {
@@ -29,13 +23,11 @@ int main()
         result2 = logicalFragmentResult(fragment2, vars);
         if (result1 != result2)
         {
-            printf("\nNot equal");
-            return 0;
+            return "\nNot equal";
         }
         else if (result1 == '\0')
         {
-            printf("\nIncorrect data");
-            return 0;
+            return "\nIncorrect data";
         }
         for (int i = size-1; i >= 0; i--)
         {
@@ -54,6 +46,20 @@ int main()
             flag = 1;
         }
     }
-    printf("\nEqual");
-    return 0;
+    return "\nEqual";
+}
+
+int main()
+{
+    int isPassed = 1;
+    if (executeProgram("test1\\fragment1.txt", "test1\\fragment2.txt"))
+    {
+        printf("Test 1 failed");
+        isPassed = 0;
+    }
+
+    if (isPassed)
+    {
+        printf("Testing passed");
+    }
 }
