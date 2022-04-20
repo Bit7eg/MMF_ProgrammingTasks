4cebacc3f54c53531eaba83c6c0b2ebf94aca703
i.stepanko <i.stepanko@WS4214-03>
2022-04-20 12:13:20 +0700

    ThemeProgramms started again

--- ThemeProgramms/ThemeProgramms.c
+++ ThemeProgramms/ThemeProgramms.c
@@ -1,65 +1,40 @@
 ﻿#include <stdio.h>
-#include <string.h>
-#include "BinGraphLib.h"
 
-char* executeProgram(const char* fileName1, const char* fileName2) {
-    BIN_GRAPH fragment1 = fscanBinGraph(fileName1);
-    BIN_GRAPH fragment2 = fscanBinGraph(fileName2);
-    VALUE_SET vars = getVariableSet(fragment1), varsBuff = getVariableSet(fragment2);
-    if (getVarCount(vars) >= getVarCount(varsBuff))
+int isLogicalFormula(char* str)
+{
+
+}
+
+int isConjunction(char* str)
+{
+
+}
+
+int isMultiplier(char* str)
+{
+
+}
+
+int isVariable(char* str)
+{
+
+}
+
+int isSpaces(char* str) {
+
+}
+
+char* getString()
+{
+    char ch, *str;
+    scanf("%c", &ch);
+    while (ch != '\n')
     {
-        killValueSet(varsBuff);
+
     }
-    else
-    {
-        killValueSet(vars);
-        vars = varsBuff;
-    }
-    int flag = 0, size = getVarCount(vars);
-    char result1, result2;
-    while ((!flag)||(vars->value))
-    {
-        result1 = logicalFragmentResult(fragment1, vars);
-        result2 = logicalFragmentResult(fragment2, vars);
-        if (result1 != result2)
-        {
-            return "\nNot equal";
-        }
-        else if (result1 == '\0')
-        {
-            return "\nIncorrect data";
-        }
-        for (int i = size-1; i >= 0; i--)
-        {
-            if (vars[i].value)
-            {
-                vars[i].value = 0;
-            }
-            else
-            {
-                vars[i].value = 1;
-                break;
-            }
-        }
-        if (vars->value)
-        {
-            flag = 1;
-        }
-    }
-    return "\nEqual";
 }
 
 int main()
 {
-    int isPassed = 1;
-    if (executeProgram("test1\\fragment1.txt", "test1\\fragment2.txt"))
-    {
-        printf("Test 1 failed");
-        isPassed = 0;
-    }
-
-    if (isPassed)
-    {
-        printf("Testing passed");
-    }
+    
 }
