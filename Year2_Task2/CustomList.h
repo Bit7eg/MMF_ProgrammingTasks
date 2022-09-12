10e58b6f4747b22816ecd7dbe0884d7f802374ac
Bit7eg <stonysyrop@gmail.com>
2022-09-12 22:43:39 +0700

    Writen code for task2

--- Year2_Task2/CustomList.h
+++ Year2_Task2/CustomList.h
@@ -1,13 +1,27 @@
 #pragma once
 
-template<class T>
+#include <iostream>
+#include <exception>
+
+template<typename T>
 class CustomList{
 private:
     struct Node {
         T data;
         Node* next;
-        Node(T value);
-        ~Node();
+
+        Node(T value) {
+            std::cout << "\nINFO::CustomList::Node::Constructor(T)\n";
+
+            this->data = value;
+            this->next = nullptr;
+        }
+
+        ~Node() {
+            std::cout << "\nINFO::CustomList::Node::Destructor()\n";
+
+            if (this->next != nullptr) delete this->next;
+        }
     };
     Node* head;
     unsigned int _length;
@@ -18,35 +32,184 @@ public:
     class Iterator {
     private:
         Node* current;
-        Iterator();
+        Iterator() {
+            std::cout << "\nINFO::CustomList::Iterator::Constructor()\n";
+
+            this->current = nullptr;
+        }
     public:
-        Iterator(const Iterator& iter);
+        Iterator(const Iterator& iter) {
+            std::cout << "\nINFO::CustomList::Iterator::Constructor(const Iterator&)\n";
 
-        Iterator& operator++();
-        Iterator operator++(int);
+            this->current = iter.current;
+        }
 
-        T operator*();
-        T* operator->();
+        Iterator& operator++() {
+            std::cout << "\nINFO::CustomList::Iterator::operator{++this}\n";
 
-        bool operator==(Iterator iter);
-        bool operator!=(Iterator iter);
+            if (this->current == nullptr) throw std::exception("No more elements in the list");
+            this->current = this->current->next;
+            return *this;
+        }
+
+        Iterator operator++(int) {
+            std::cout << "\nINFO::CustomList::Iterator::operator{this++}\n";
+
+            if (this->current == nullptr) throw std::exception("No more elements in the list");
+            this->current = this->current->next;
+            return *this;
+        }
+
+        T operator*() {
+            std::cout << "\nINFO::CustomList::Iterator::operator*\n";
+
+            return this->current->data;
+        }
+
+        T* operator->() {
+            std::cout << "\nINFO::CustomList::Iterator::operator->\n";
+
+            return &(this->current->data);
+        }
+
+        bool operator==(Iterator iter) {
+            std::cout << "\nINFO::CustomList::Iterator::operator{==Iterator}\n";
+
+            return this->current == iter.current;
+        }
+
+        bool operator!=(Iterator iter) {
+            std::cout << "\nINFO::CustomList::Iterator::operator{!=Iterator}\n";
+
+            return this->current != iter.current;
+        }
     };
 
-    CustomList();
-    ~CustomList();
+    CustomList() {
+        std::cout << "\nINFO::CustomList::Constructor()\n";
+
+        this->head = nullptr;
+    }
+
+    ~CustomList() {
+        std::cout << "\nINFO::CustomList::Destructor()\n";
+
+        if (this->head != nullptr) delete this->head;
+    }
     
-    CustomList push(T data);
-    CustomList push_back(T data);
+    CustomList push(T data) {
+        std::cout << "\nINFO::CustomList::push(T data)\n";
+
+        Node* curr;
+        try
+        {
+            curr = new Node(data);
+        }
+        catch (const std::exception& e)
+        {
+            /* TODO: response to memory overflow */
+            std::cerr << "\nERROR::" << e.what() << '\n';
+        }
+        curr->next = this->head;
+        this->head = curr;
+        this->_length++;
+        return *this;
+    }
+
+    CustomList push_back(T data) {
+        std::cout << "\nINFO::CustomList::push_back(T data)\n";
+
+        Node* curr = this->head;
+        while (curr->next != nullptr) {
+            curr = curr->next;
+        }
+        try
+        {
+            curr->next = new Node(data);
+        }
+        catch (const std::exception& e)
+        {
+            /* TODO: response to memory overflow */
+            std::cerr << "\nERROR::" << e.what() << '\n';
+        }
+        this->_length++;
+        return *this;
+    }
     
-    T pop();
-    T pop_back();
+    T pop() {
+        std::cout << "\nINFO::CustomList::pop()\n";
 
-    unsigned int length();
+        if (this->head == nullptr) throw std::exception("List is empty");
+        T data = this->head->data;
+        Node* curr = this->head;
+        this->head = curr->next;
+        curr->next = nullptr;
+        delete curr;
+        this->_length--;
+        return data;
+    }
 
-    Iterator begin();
-    Iterator getIterator(const unsigned int position);
-    Iterator end();
+    T pop_back() {
+        std::cout << "\nINFO::CustomList::pop()\n";
 
-    T operator[](const int index) const;
-    T& operator[](const int index);
+        if (this->head == nullptr) throw std::exception("List is empty");
+        if (this->head->next == nullptr) {
+            T data = this->head->data;
+            delete this->head;
+            this->head = nullptr;
+            return data;
+        }
+
+        Node* curr = this->head;
+        while (curr->next->next != nullptr) {
+            curr = curr->next;
+        }
+        T data = curr->next->data;
+        delete curr->next;
+        curr->next = nullptr;
+        this->_length--;
+        return data;
+    }
+
+    unsigned int length() {
+        std::cout << "\nINFO::CustomList::length()\n";
+
+        return this->_length;
+    }
+
+    Iterator begin() const {
+        std::cout << "\nINFO::CustomList::begin()const\n";
+
+        Iterator iter;
+        iter.current = this->head;
+        return iter;
+    }
+
+    Iterator getIterator(const unsigned int position) const {
+        std::cout << "\nINFO::CustomList::getIterator(const unsigned int)const\n";
+
+        if (this->head == nullptr) throw std::exception("List is empty");
+        if (position == 0) return this->begin();
+        Iterator iter;
+        iter.current = this->head;
+        for (size_t i = 1; iter.current->next != nullptr; i++)
+        {
+            iter++;
+            if (i == position) return iter;
+        }
+        throw std::exception("Not enough elements in the list");
+    }
+
+    Iterator end() const {
+        std::cout << "\nINFO::CustomList::end()const\n";
+
+        return Iterator();
+    }
+
+    T& operator[](const int index) {
+        std::cout << "\nINFO::CustomList::operator[const int]\n";
+
+        if (index < 0) throw std::exception("Incorrect index");
+        return this->getIterator(index).current->data;
+    }
 };
--- Year2_Task2/Year2_Task2.cpp
+++ Year2_Task2/Year2_Task2.cpp
@@ -2,7 +2,37 @@
 
 #include "CustomList.h"
 
+struct IntContainer {
+    int data;
+};
+
 int main()
 {
-    /* TODO: library testing */
+    CustomList<int> intList;
+    CustomList<IntContainer> containerList;
+
+    std::cout << "\n__________________________________________________________\n\n\n";
+
+    int val = intList.push(1).push(2).pop();
+    std::cout << "List contains: { ";
+    for (CustomList<int>::Iterator iter = intList.begin(); iter != intList.end(); iter++)
+    {
+        std::cout << *iter << ", ";
+    }
+    std::cout << "\nGets from intList: " << val << "\n";
+
+    std::cout << "\n__________________________________________________________\n\n\n";
+
+    IntContainer valContainer = containerList.push_back({ 2 }).push_back({ 1 }).pop_back();
+    std::cout << "List contains: { ";
+    for (CustomList<IntContainer>::Iterator iter = containerList.begin(); iter != containerList.end(); ++iter)
+    {
+        std::cout << iter->data << ", ";
+    }
+    std::cout << "\nGets from list: " << valContainer.data << "\n";
+
+    //CustomList<int> copiedList = intList;
+    //copiedList = list;
+
+    std::cout << "\n__________________________________________________________\n\n\n";
 }
